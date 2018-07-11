b58035b845afb2c45e346a6dbcc3c0019186a444
jumpman024 <konstantin.bezsmertnyy@gmail.com>
2018-07-11 17:37:19 +0300

    my commit

---
+++ js/login-form.js
@@ -0,0 +1,8 @@
+function check(form){
+    if(form.username.value == "fucku" && form.password.value == "abc"){
+        alert("login successful")
+    } else {
+        alert("incorrect credentials")
+    }
+}
+
